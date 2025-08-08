eedd55c5fb99c6aa3f38c13e8ce44ad591d2f240
jeraaa <hiljeragarcia11-ops>
2025-08-08 21:12:21 +0800

    feat(Jera): add Jera class with properties and describe method

---
+++ lib/jera.dart
@@ -0,0 +1,10 @@
+class Jera {
+  String little;
+  String jehh;
+
+  Jera({required this.little, required this.jehh});
+
+  void describe() {
+    print('The book "$little" is written by $jehh.');
+  }
+}
