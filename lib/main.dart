aa083195051bf1f848d2055a2b04528d26f5f1ad
daffle05 <torlaodafny@gmail.com>
2025-08-10 23:22:16 +0800

    Fix Student class age type issue and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'book.dart';
-import 'jera.dart';
+import 'student.dart';
 
 void main() {
   var b1 = Book(title: '1984', author: 'George Orwell');
   b1.describe();
 
-  var j2 = Jera(little: 'hannah', jehh: 'banana' );
-  j2.describe();
+  var s2 = Student(name: 'hannah', age: int.parse('19'));
+  s2.introduce();
 }
--- lib/student.dart
+++ lib/student.dart
@@ -2,9 +2,7 @@ class Student {
   String name;
   int age;
 
-  Student(this.name, this.age);
-  // Or use named parameters:
-  // Student({required this.name, required this.age});
+  Student({required this.name, required this.age});
 
   void introduce() {
     print('Hi, I am $name and I am $age years old.');
